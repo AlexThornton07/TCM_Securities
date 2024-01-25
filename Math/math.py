3e2142a5a7c9a54f02658cd70e8281e6b497af5f
Thornton <alexander.thornton07@gmail.com>
2024-01-25 09:02:41 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,12 @@
+#Programmer: Alex Thornton
+#Program: Math
+#Date: 1.10.2024
+
+print(9 + 9) #Addition
+print(9 - 9) #Subtraction
+print(9 * 9) #Multiplication
+print(9 / 9) #Division
+print(9 ** 9) #Exponet
+print(9 % 9) #Modulo - takes whats left over
+print(9 / 6) #Divsion with remainder  
+print(9 // 6) #Whole Number with no remainder
