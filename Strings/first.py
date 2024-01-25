997075c258235a2bc0e7be22dfd7cd6abd33e3fc
Thornton <alexander.thornton07@gmail.com>
2024-01-25 09:01:39 -0500

    Strings

---
+++ Strings/first.py
@@ -0,0 +1,13 @@
+#!/bin/python3 
+
+#Programmer: Alex Thornton
+#Program: Strings 
+
+print("Sully is a Cornball\n")
+
+print("67 * 21 =", 67*21)
+print("")
+print("1525 / 57 = "+ str(1525/57))
+print("")
+print("""Happy New Year!
+everyone!!!""") #Triple quote for multi-line print 
